2182d1f24c103fb67fa3234760f32576d56cd2f1
Grigor <goqorkeropyan@gmail.com>
2020-04-30 15:38:30 +0400

    Corrected comp test file

--- comp_test.py
+++ comp_test.py
@@ -3,4 +3,7 @@ import pyomp
 print("aaaaaaaaaaaaa")
 print(dir(pyomp))
 
-print(pyomp.py_eval(1, 2, 3, 4, 5, 6, 7))
+print(pyomp.eval7(1, 2, 3, 4, 5, 6, 7))
+print(pyomp.eval6(1, 2, 3, 4, 5, 6))
+print(pyomp.eval5(1, 2, 3, 4, 5))
+
