7a7bba59faff9c49ea4d21d6318dcd5160b6f694
Davit Khechoyan <davkhech@gmail.com>
2020-05-04 13:34:24 +0400

    build sources and move shared lib

--- setup.py
+++ setup.py
@@ -1,6 +1,19 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+
+
+class PreInstall(build_ext):
+    @staticmethod
+    def script(directory):
+        from subprocess import call
+        call(['make'], cwd='.')
+
+    def run(self):
+        self.execute(PreInstall.script, (self.build_lib,), msg="Running pre install script")
+        build_ext.run(self)
+
 
 examples_extension = Extension(
     name="pyomp",
@@ -12,6 +25,10 @@ examples_extension = Extension(
 
 setup(
     name="pyomp",
-    ext_modules=cythonize([examples_extension])
+    ext_modules=cythonize([examples_extension]),
+    cmdclass={
+        'build_ext': PreInstall,    
+    },
+    version='0.0.1',
 )
 
